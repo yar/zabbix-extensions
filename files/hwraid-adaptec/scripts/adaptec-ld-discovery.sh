6c10852c8c1fdd172d81457537299a6c707247d0
yar <yar.dmitriev@gmail.com>
2018-10-10 13:11:20 +0300

    Update adaptec-ld-discovery.sh
    Perform case-insensitive output match to be compatible with arcconf 2.04 for Adaptec RAID

--- files/hwraid-adaptec/scripts/adaptec-ld-discovery.sh
+++ files/hwraid-adaptec/scripts/adaptec-ld-discovery.sh
@@ -3,7 +3,7 @@
 # Description:	Logical drives auto-discovery via arcconf. (TESTED WITH V5.20 (B17414))
 
 adp_list=$(sudo arcconf getversion |grep -w "^Controller" |cut -d# -f2)
-ld_list=$(for a in $adp_list; do sudo arcconf getconfig $a ld |grep -w "Logical device number" |awk '{print $4}' |while read ld ; do echo $a:$ld; done ; done)
+ld_list=$(for a in $adp_list; do sudo arcconf getconfig $a ld |grep -w -i "Logical device number" |awk '{print $4}' |while read ld ; do echo $a:$ld; done ; done)
 
 if [[ $1 = raw ]]; then
   for ld in ${ld_list}; do echo $ld; done ; exit 0
